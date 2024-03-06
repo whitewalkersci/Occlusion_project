693325478aaea0bac23b73c56d49b4318c120756
whitewalker11 <santoshwar.dhruv11@gmail.com>
2024-03-06 13:27:09 +0530

    changes in code

--- flask_api.py
+++ flask_api.py
@@ -9,14 +9,12 @@ from pathlib import Path
 import sqlite3
 import json 
 from datetime import datetime
- 
+
 # datetime object containing current date and time
 now = datetime.now()
 
 app = Flask(__name__)
 api = Api(app)
-
-
 
 def update_json_file(filename, key, new_dict):
     try:
@@ -36,29 +34,23 @@ def update_json_file(filename, key, new_dict):
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
 
-
 class SectionProcessing(Resource):
     def post(self):
         try:
-            # Get the image path from the request JSON
             data = request.get_json()
             image_path = data.get('image_path')
             if not image_path:
                 return {'message': 'No image path provided'}, 400
 
-            # Check if the image path has the correct format and naming template
             image_filename = os.path.basename(image_path)
             if not image_filename.endswith(".tif"):
                 return {'message': 'Invalid image path. Image filename should start with "Specimen_" and have a .tif extension'}, 400
 
-            # Process the image
             model_section_path = os.path.join(os.getcwd(),'models', 'section.onnx')
             image_processor = ImageProcessor(model_section_path)
 
-            specimen_name = image_filename.split('.')[0]  # Using the filename directly since we've already validated it
+            specimen_name = image_filename.split('.')[0]
             image, crops = image_processor.get_sections(image_path, specimen_name)
-
-            # Perform any further processing or return the results as needed
 
             return {'message': 'Image processed successfully','specimen_name':specimen_name, 'sections_image_path': crops}, 200
         except Exception as e:
@@ -82,7 +74,6 @@ class CountProcessing(Resource):
     
             specimen_name = data['specimen_name']
         
-
             sections_image_path = data['sections_image_path']  
             result[specimen_name]= {}
 
@@ -94,11 +85,9 @@ class CountProcessing(Resource):
                 section_image_copy = sections_image_path.copy()
                 patch = process_pillar.process_cont(section_image, sec_id, specimen_name)
 
-                # Detect pillars in the section
                 image, pillar_count, all_points, plotted_image_path = process_pillar.detect_pillars(section_image, patch, sec_id,
                                                                                 specimen_name)
 
-                # Find occlusions in the section
                 num_negative, num_positive = occlusion_model.occlusion_finder(all_points, section_image, sec_id,
                                                                               specimen_name)
 
@@ -114,7 +103,6 @@ class CountProcessing(Resource):
             result[specimen_name]['Occlusion_index'] = 0
             result[specimen_name]['Date_time'] = now.strftime("%d/%m/%Y-%H:%M:%S")
 
-
             with open(database_filename, 'r') as file:
                 data = json.load(file)
                 total_elements = sum(len(value) if isinstance(value, list) else 1 for value in data.values())
@@ -124,9 +112,7 @@ class CountProcessing(Resource):
             with open(database_filename, 'w') as file:
                 json.dump(data, file, indent=4)
 
-
             return result , 200
-
         except Exception as e:
             import traceback
             traceback.print_exc()
@@ -161,14 +147,9 @@ class ViewResult(Resource):
         except:
             return {'message': f'Error reading {str(e)}'}, 500
 
-
-# Function to create a new database connection
 def get_db():
-    # Check if the database file exists
     if not os.path.exists('database.db'):
-        # If the database file doesn't exist, create it
         conn = sqlite3.connect('database.db')
-        # Create the users table
         conn.execute('''
             CREATE TABLE users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -180,8 +161,6 @@ def get_db():
         conn.close()
 
     return sqlite3.connect('database.db')
-
-
 
 class ResultShow(Resource):
     def post(self):
@@ -218,27 +197,21 @@ class ResultShow(Resource):
             return {'message': f'Error reading {str(e)}'}, 500
         
 
-
-
 class Login(Resource):
     def post(self):
         try:
-            # Get username and password from request JSON
             data = request.get_json()
             username = data.get('username')
             password = data.get('password')
 
-            # Create a new database connection and cursor within the current thread
             with get_db() as conn:
                 cur = conn.cursor()
 
-                # Query the database to check if the user exists
                 cur.execute("SELECT * FROM users WHERE username = ?", (username,))
                 user = cur.fetchone()
 
                 if user:
-                    # Check if the provided password matches the password in the database
-                    if password == user[2]:  # Assuming password is stored in the third column
+                    if password == user[2]:
                         return {'message': 'Login successful'}, 200
                     else:
                         return {'message': 'Invalid username or password'}, 401
@@ -250,40 +223,32 @@ class Login(Resource):
 class CreateUser(Resource):
     def post(self):
         try:
-            # Get username and password from request JSON
             data = request.get_json()
             username = data.get('username')
             password = data.get('password')
 
-            # Create a new database connection and cursor within the current thread
             with get_db() as conn:
                 cur = conn.cursor()
 
-                # Check if the username already exists in the database
                 cur.execute("SELECT * FROM users WHERE username = ?", (username,))
                 existing_user = cur.fetchone()
 
                 if existing_user:
                     return {'message': 'Username already exists. Please choose a different username.'}, 400
                 else:
-                    # Insert the new user into the database
                     cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
                     conn.commit()
                     return {'message': 'User created successfully'}, 201
         except Exception as e:
             return {'message': f'Error creating user: {str(e)}'}, 500
-        
 
-
-# Add the Login and CreateUser resources to the API
 api.add_resource(Login, '/login')
 api.add_resource(CreateUser, '/create_user')
 
-# Define the API endpoint
 api.add_resource(SectionProcessing, '/process_image')
 api.add_resource(CountProcessing,'/process_analysis')
 api.add_resource(HistoryPage, '/history')
 api.add_resource(ResultShow, '/result')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)  # Set debug to False in production
+    app.run(debug=True,port=5000)
